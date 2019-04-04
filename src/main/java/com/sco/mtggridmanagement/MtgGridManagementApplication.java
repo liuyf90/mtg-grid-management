0a59054745b18e53a3877d0e5c9c3000124c7266
刘宇飞 <liuyf90@gmail.com>
2019-04-04 09:48:30 +0800

    Initial commit

---
+++ src/main/java/com/sco/mtggridmanagement/MtgGridManagementApplication.java
@@ -0,0 +1,16 @@
+package com.sco.mtggridmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 刘宇飞 on 2019-04-03.
+ */
+@SpringBootApplication
+public class MtgGridManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgGridManagementApplication.class, args);
+    }
+
+}
